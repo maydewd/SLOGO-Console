f043d7a22ad59c6e3e04f4b125c892b9160bd020
Nick Lockett <NickS.Lockett@gmail.com>
2016-02-23 15:42:20 -0500

    correction of error

--- src/app/FrontEndControllerInterface.java
+++ src/app/FrontEndControllerInterface.java
@@ -3,11 +3,11 @@ package app;
 import javafx.scene.paint.Color;
 
 public interface FrontEndControllerInterface {
-	public Object setPenColor(Color c);
+	public void setPenColor(Color c);
 
-	public Object setBackgroundColor(Color black);
+	public void setBackgroundColor(Color black);
 
-	public Object setLanguage(String string);
+	public void setLanguage(String string);
 
 	public Color getPenColor();
 
