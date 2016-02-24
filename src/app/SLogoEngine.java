55496a73c0e4788361e642f7cc68ff35c0d22fd5
David Maydew <dmm68@duke.edu>
2016-02-24 14:49:08 -0500

    Add example initialization to Engine to connect later

--- src/app/SLogoEngine.java
+++ src/app/SLogoEngine.java
@@ -2,18 +2,23 @@ package app;
 
 import controller.FrontEndControllerInterface;
 import javafx.stage.Stage;
+import model.IBasicModel;
+import model.SLogoBasicModel;
 import view.UIManager;
+
 
 /**
  * Created by Tim on 18/02/16.
  */
 public class SLogoEngine {
 
-	private UIManager uiManager;
-	private FrontEndControllerInterface controller;
+    private UIManager uiManager;
+    private FrontEndControllerInterface controller;
 
-	public void start(Stage stage) {
-		uiManager = new UIManager(stage, controller);
+    public void start (Stage stage) {
+        IBasicModel basicModel = new SLogoBasicModel();
+        // TODO pass this model to UIManager
+        uiManager = new UIManager(stage, controller);
 
-	}
+    }
 }
