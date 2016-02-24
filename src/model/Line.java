cbde0baa5f20c030d147cdc86519fca295801c3b
David Maydew <dmm68@duke.edu>
2016-02-24 02:13:14 -0500

    Add basic line class

---
+++ src/model/Line.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Line {
+    
+    private Point myStart;
+    private Point myEnd;
+
+    public Line (Point start, Point end) {
+        setStart(start);
+        setEnd(end);
+    }
+
+    public Point getStart () {
+        return myStart;
+    }
+
+    public void setStart (Point start) {
+        myStart = start;
+    }
+
+    public Point getEnd () {
+        return myEnd;
+    }
+
+    public void setEnd (Point end) {
+        myEnd = end;
+    }
+
+}
