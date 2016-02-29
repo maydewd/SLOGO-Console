5ada3f45fd07230bdfc382afc938e86650ff5aba
David Maydew <dmm68@duke.edu>
2016-02-29 10:55:37 -0500

    Add color and movement capabilities to Turtle, change to primitives

--- src/model/Turtle.java
+++ src/model/Turtle.java
@@ -1,46 +1,89 @@
 package model;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.Property;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import java.util.List;
+
 
 public class Turtle {
-	
-	private DoubleProperty myHeading;
-	private Point myLocation;
-	private BooleanProperty myPenDown;
-	private BooleanProperty myVisibility;
-	private StringProperty myImage;
-	
-	public Turtle(double h, Point l, boolean pd, boolean v, String i){
-		this.myHeading = new SimpleDoubleProperty(h);
-		this.myLocation = l;
-		this.myPenDown = new SimpleBooleanProperty(pd);
-		this.myVisibility = new SimpleBooleanProperty(v);
-		this.myImage = new SimpleStringProperty(i);
-	}
-	
-	public DoubleProperty getHeading() {
-		return myHeading;
-	}
-	
-	public Point getLocation(){
-		return myLocation;
-	}
 
-	public BooleanProperty getPenDown() {
-		return myPenDown;
-	}
-	
-	public BooleanProperty getVisibility() {
-		return myVisibility;
-	}
-	
-	public StringProperty getImage(){
-		return myImage;
-	}
+    private double myHeading = 0;
+    private Point myLocation = new Point(0, 0);
+    private boolean isPenDown = true;
+    private int myPenColorIndex;
+    private boolean isVisible = true;
+    private int myImageIndex;
+
+    public Turtle (int penColorIndex, int imageIndex) {
+        setMyPenColorIndex(penColorIndex);
+        setMyImageIndex(imageIndex);
+    }
+
+    public Turtle (double heading,
+                   Point location,
+                   boolean penDown,
+                   int penColorIndex,
+                   boolean visible,
+                   int imageIndex) {
+        setHeading(heading);
+        setLocation(location);
+        setPenDown(penDown);
+        setMyPenColorIndex(penColorIndex);
+        setVisible(visible);
+        setMyImageIndex(imageIndex);
+    }
+
+    public double getHeading () {
+        return myHeading;
+    }
+
+    public void setHeading (double heading) {
+        myHeading = heading;
+    }
+
+    public Point getLocation () {
+        return myLocation;
+    }
+
+    public List<Line> setLocation (Point location) {
+        myLocation = location;
+        // TODO Add in line creation
+        return null;
+    }
+    
+    public List<Line> moveForward (double pixels) {
+        // TODO implement and add line creation
+        return null;
+    }
+
+    public boolean isPenDown () {
+        return isPenDown;
+    }
+
+    public void setPenDown (boolean penDown) {
+        isPenDown = penDown;
+    }
+
+    public int getPenColorIndex () {
+        return myPenColorIndex;
+    }
+
+    public void setMyPenColorIndex (int penColorIndex) {
+        myPenColorIndex = penColorIndex;
+    }
+
+    public boolean isVisible () {
+        return isVisible;
+    }
+
+    public void setVisible (boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+
+    public int getImageIndex () {
+        return myImageIndex;
+    }
+
+    public void setMyImageIndex (int imageIndex) {
+        myImageIndex = imageIndex;
+    }
+
 }
