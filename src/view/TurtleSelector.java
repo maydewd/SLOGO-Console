09ecc28b0f1347d5ac764f37bf072a682fc79c22
torrescar <cmt57@duke.edu>
2016-03-11 19:38:23 -0500

    added change listener for turtle selector

--- src/view/TurtleSelector.java
+++ src/view/TurtleSelector.java
@@ -1,5 +1,7 @@
 package view;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
@@ -10,11 +12,27 @@ public class TurtleSelector extends Selector {
 
     public TurtleSelector (IBasicModel basicModel) {
         turtleSettings = new Menu("Turtle Selector");
-        for(int i = 0; i < basicModel.turtleImageOptionsProperty().getSize(); i++){
+        initializeMenu(basicModel);
+        createInvalidationListener(basicModel);
+    }
+
+    private void createInvalidationListener (IBasicModel basicModel) {
+        basicModel.turtleImageOptionsProperty().addListener(new InvalidationListener() {
+            @Override
+            public void invalidated (Observable o) {
+                initializeMenu(basicModel);
+            }
+        });
+    }
+    
+    private void initializeMenu (IBasicModel basicModel) {
+        getMenu().getItems().clear();
+        for (int i=0; i < basicModel.turtleImageOptionsProperty().getSize(); i++) {
             makeButton(i, basicModel);
         }
     }
 
+    
     public void makeButton (int index, IBasicModel basicModel) {
         MenuItem button = new MenuItem(String.valueOf(index));
         button.setGraphic(new ImageView(basicModel.turtleImageOptionsProperty().get(index)));
