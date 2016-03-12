a3a0e4513afc17e9fa1036ab1a7828894e125fe0
Nick Lockett <NickS.Lockett@gmail.com>
2016-03-12 00:18:08 -0500

    advanced help button

--- src/view/HelpSelector.java
+++ src/view/HelpSelector.java
@@ -5,14 +5,17 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
 
 public class HelpSelector extends Selector {
-    public static final String HELP_PAGE = "https://www.cs.duke.edu/courses/compsci308/spring16/assign/03_slogo/commands.php";
+    public static final String HELP_PAGE_1 = "https://www.cs.duke.edu/courses/compsci308/spring16/assign/03_slogo/commands.php";
+    public static final String HELP_PAGE_2 = "https://www.cs.duke.edu/courses/compsci308/spring16/assign/03_slogo/commands2_J2W.php";
     private Menu myHelpSelector;
 
     public HelpSelector (HostServices hostServices) {
         myHelpSelector = new Menu("Help");
-        MenuItem help = new MenuItem("Get Help");
-        help.setOnAction(e -> hostServices.showDocument(HELP_PAGE));
-        myHelpSelector.getItems().addAll(help);
+        MenuItem help = new MenuItem("Initial Commands");
+        help.setOnAction(e -> hostServices.showDocument(HELP_PAGE_1));
+        MenuItem help2 = new MenuItem("Advanced Commands");
+        help.setOnAction(e -> hostServices.showDocument(HELP_PAGE_2));
+        myHelpSelector.getItems().addAll(help, help2);
     }
     
     @Override
